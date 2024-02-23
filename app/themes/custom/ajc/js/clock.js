06e56fc5b41f4089a28ba4ea0e5948d1bf3659f1
unknown <opusidea@gmail.com>
2024-02-23 16:38:56 +0100

    qcm

--- app/themes/custom/ajc/js/clock.js
+++ app/themes/custom/ajc/js/clock.js
@@ -1,1 +1,24 @@
-console.log("clock.js loaded !")
+//console.log("clock.js loaded !")
+
+
+// pattern d'un Drupal behavior
+(function(Drupal, $) { 
+    
+    Drupal.behaviors.demoClock = {
+        attach: function(context, settings) {
+
+            function ticker() {
+                var date = new Date();
+                $(context).find('#clock').text(date);
+            }
+
+            setInterval(ticker, 1000); // DOM update every second
+
+        }
+    }
+
+})(Drupal, jQuery)
+
+
+
+
